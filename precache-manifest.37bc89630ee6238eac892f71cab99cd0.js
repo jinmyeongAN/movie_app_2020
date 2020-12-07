85d3eafb800837a6182c217bd43a9bf3757d3581
jinmyeongAN <jinmyeong.an@gmail.com>
2020-12-07 13:56:28 +0000

    Updates

---
+++ precache-manifest.37bc89630ee6238eac892f71cab99cd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "915a0ff0bb057118024cd690f8d8ae60",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "56b49c2a4b8b295d9f5b",
+    "url": "/movie_app_2020/static/js/2.db1e8dfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.db1e8dfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de02508ea636f7b8e9be",
+    "url": "/movie_app_2020/static/js/main.c80762de.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
